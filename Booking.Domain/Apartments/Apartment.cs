242453c92696d3415ccb60e06663ce4656ef88e2
Bohdan <bohdan_voroniak@epam.com>
2025-04-20 18:39:46 +0300

    add apartament domain object

---
+++ Booking.Domain/Apartments/Apartment.cs
@@ -0,0 +1,25 @@
+﻿using Booking.Domain.Abstractions;
+
+namespace Booking.Domain.Apartments;
+
+public sealed class Apartment : Entity
+{
+    public Apartment(Guid id) : base(id)
+    {
+        
+    }
+    
+    public Name Name { get; private set; }
+
+    public Description Description { get; private set; }
+
+    public Address Address { get; private set; }
+    
+    public Money Price { get; private set; }
+    
+    public Money CleaningFee { get; private set; }
+
+    public DateTime? LastBookedOnUtc { get; private set; }
+
+    public List<Amenity> Amenities { get; private set; } = new();
+}
